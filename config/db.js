ed92352b37c7d10ab47f03abe7ae6e89d4d984b6
jishnu anil <jishnuanil255@gmail.com>
2024-10-15 08:41:23 +0530

    db uri updated

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect('mongodb+srv://jisanilart:zkf4UtV53pTRpLDg@articlegram.4ldpg.mongodb.net/?retryWrites=true&w=majority&appName=Articlegram')
     .then(() => {
       console.log("Mongodb connected successfully");
     })
